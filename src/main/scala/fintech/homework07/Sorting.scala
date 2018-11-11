784edc7af1df293f0e8779d9011aa34101757b4a
MIkhail Karpenko <krosh347@gmail.com>
2018-11-11 19:47:44 +0500

    second commit

--- src/main/scala/fintech/homework07/Sorting.scala
+++ src/main/scala/fintech/homework07/Sorting.scala
@@ -1,7 +1,6 @@
 package fintech.homework07
 
 import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
 
 /**
   * Реализовать алгоритмы quick-sort и merge-sort
@@ -9,66 +8,75 @@ import scala.collection.mutable.ArrayBuffer
   */
 
 object Sorting {
-  def mergeSort[T](data: Seq[T])(implicit ord: Ordering[T]): Seq[T] = {
+  type MutSeq[T] = mutable.Seq[T]
+  
+  def mergeSort[T](collection: MutSeq[T])(implicit ord: Ordering[T]): Unit = {
     import ord._
 
-    def merge(arr: ArrayBuffer[T], left: Int, mid: Int, right: Int) {
+    def merge(left: Int, mid: Int, right: Int) {
       var ind1 = left
       var ind2 = mid
       val result = mutable.Queue[T]()
 
       for (_ <- left until right) {
-        if (ind1 < mid && (ind2 >= right || arr(ind1) < arr(ind2))) {
-          result.enqueue(arr(ind1))
+        if (ind1 < mid && (ind2 >= right || collection(ind1) < collection(ind2))) {
+          result.enqueue(collection(ind1))
           ind1 += 1
         } else {
-          result.enqueue(arr(ind2))
+          result.enqueue(collection(ind2))
           ind2 += 1
         }
       }
       for(i <- left until right)
-        arr(i) = result.dequeue()
+        collection(i) = result.dequeue()
     }
 
-    val arr = data.to[ArrayBuffer]
-    for(i <- Iterator.iterate(1)(_ * 2).takeWhile(_ < arr.length);
-        j <- arr.indices by 2 * i)
-        merge(arr, j, j + i, math.min(j + 2 * i, arr.length))
-    arr
-  }
-
-  def quickSort[T](data: Seq[T])(implicit ord: Ordering[T]): Seq[T] = {
-    import ord._
-
-    def recQuickSort(arr: ArrayBuffer[T], lowerBound: Int, upperBound: Int): Unit = {
-      if (lowerBound < upperBound){
-        val part = partition(arr, lowerBound, upperBound)
-        recQuickSort(arr, lowerBound, part)
-        recQuickSort(arr, part + 1, upperBound)
+    def mergeSortRec(left: Int, right: Int): Unit = {
+      if (right - left > 1) {
+        val mid = (left + right) / 2
+        mergeSortRec(left, mid)
+        mergeSortRec(mid, right)
+        merge(left, mid, right)
       }
     }
 
-    def partition(arr: ArrayBuffer[T], lowerBound: Int, upperBound: Int): Int = {
-      val pivot = arr(lowerBound)
+    mergeSortRec(0, collection.length)
+//    for(i <- Iterator.iterate(1)(_ * 2).takeWhile(_ < collection.length);
+//        j <- collection.indices by 2 * i)
+//        merge(j, j + i, math.min(j + 2 * i, collection.length))
+//    collection
+  }
+
+  def quickSort[T](collection: MutSeq[T])(implicit ord: Ordering[T]): Unit = {
+    import ord._
+
+    def quickSortRec(lowerBound: Int, upperBound: Int): Unit = {
+      if (lowerBound < upperBound){
+        val part = partition(lowerBound, upperBound)
+        quickSortRec(lowerBound, part)
+        quickSortRec(part + 1, upperBound)
+      }
+    }
+
+    def partition(lowerBound: Int, upperBound: Int): Int = {
+      val pivot = collection(lowerBound)
       var ind1 = lowerBound - 1
       var ind2 = upperBound + 1
       while (ind1 < ind2) {
-        do ind1 += 1 while(arr(ind1) < pivot)
-        do ind2 -= 1 while(arr(ind2) > pivot)
+        do ind1 += 1 while(collection(ind1) < pivot)
+        do ind2 -= 1 while(collection(ind2) > pivot)
         if (ind1 < ind2)
-          swap(arr, ind1, ind2)
+          swap(ind1, ind2)
       }
       ind2
     }
 
-    def swap(arr: ArrayBuffer[T], ind1: Int, ind2: Int): Unit = {
-      val element = arr(ind1)
-      arr(ind1) = arr(ind2)
-      arr(ind2) = element
+    def swap(ind1: Int, ind2: Int): Unit = {
+      val element = collection(ind1)
+      collection(ind1) = collection(ind2)
+      collection(ind2) = element
     }
 
-    val array = data.to[ArrayBuffer]
-    recQuickSort(array, 0, data.length - 1)
-    array
+    quickSortRec(0, collection.length - 1)
   }
 }
--- src/test/scala/fintech/homework07/SortingSpec.scala
+++ src/test/scala/fintech/homework07/SortingSpec.scala
@@ -2,48 +2,90 @@ package fintech.homework07
 import java.time.LocalDate
 
 import org.scalatest.{FlatSpec, Matchers}
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable
 
 class SortingSpec extends FlatSpec with Matchers {
   case class Person(name: String, birthDate: LocalDate)
 
-  val people = List(
-    Person("Bob", LocalDate.of(1981, 5, 12)),
-    Person("Charlie", LocalDate.of(1982, 5, 11)),
-    Person("Alice", LocalDate.of(1970, 1, 1)),
-    Person("Michael", LocalDate.of(1978, 5, 12))
-  )
-  val listInteger = List(1, 7, 5, 5, 12, 2, 10)
-  val listString = List("def", "abb", "abcd", "a", "ab")
-  val listChar = List('a', 'f', 'm', 'b', 'c', 'e', 'f')
-  val seqInteger = Seq(1, 7, 5, 5, 12, 2, 10)
-  val sortedSeq = Seq(1, 2, 3)
+  "QuickSort" should "correct sort mutable sequences with Int" in {
+    val arrayInteger = ArrayBuffer(1, 7, 5, 5, 12, 2, 10)
+    val seqInteger = mutable.Seq(1, 7, 5, 5, 12, 2, 10)
+    val sortedSeq = mutable.Seq(1, 2, 3)
+    val emptyArray = ArrayBuffer.empty[Int]
 
-  "QuickSort" should "correct sort sequence with Int" in {
-    Sorting.quickSort(listInteger) should be (listInteger.sorted)
-    Sorting.mergeSort(List.empty) should be (List.empty)
-    Sorting.quickSort(seqInteger) should be (seqInteger.sorted)
-    Sorting.quickSort(sortedSeq) should be (sortedSeq.sorted)
+    Sorting.quickSort(arrayInteger)
+    arrayInteger should be (arrayInteger.sorted)
+    Sorting.quickSort(seqInteger)
+    seqInteger should be (seqInteger.sorted)
+    Sorting.quickSort(sortedSeq)
+    sortedSeq should be (sortedSeq.sorted)
+
+    Sorting.quickSort(emptyArray)
+    emptyArray should be (ArrayBuffer.empty)
   }
-  it should "correct sort sequence with String or Char" in {
-    Sorting.quickSort(listChar) should be (listChar.sorted)
-    Sorting.quickSort(listString) should be (listString.sorted)
+  it should "correct sort mutable sequences with String or Char" in {
+    val arrayString = ArrayBuffer("def", "abb", "abcd", "a", "ab")
+    val arrayChar = ArrayBuffer('a', 'f', 'm', 'b', 'c', 'e', 'f')
+
+    Sorting.quickSort(arrayChar)
+    arrayChar should be (arrayChar.sorted)
+    Sorting.quickSort(arrayString)
+    arrayString should be (arrayString.sorted)
   }
-  it should "correct sort sequence with Person" in {
+  it should "correct sort mutable sequences with Person" in {
     implicit val peopleOrdering: Ordering[Person] = Ordering.by(_.name)
-    Sorting.quickSort(people) should be (people.sorted)
+    val people = ArrayBuffer(
+      Person("Bob", LocalDate.of(1981, 5, 12)),
+      Person("Charlie", LocalDate.of(1982, 5, 11)),
+      Person("Alice", LocalDate.of(1970, 1, 1)),
+      Person("Michael", LocalDate.of(1978, 5, 12))
+    )
+
+    Sorting.quickSort(people)
+    people should be (people.sorted)
+  }
+  it should "correct sort mutable BIG sequences with Int" in {
+    val bigArrayInteger: ArrayBuffer[Int] = ArrayBuffer.fill(1000000)(util.Random.nextInt)
+    Sorting.quickSort(bigArrayInteger)
+    bigArrayInteger should be (bigArrayInteger.sorted)
   }
 
-  "MergeSort" should "correct sort sequence with Int" in {
-    Sorting.mergeSort(listInteger) should be (listInteger.sorted)
-    Sorting.mergeSort(seqInteger) should be (seqInteger.sorted)
-    Sorting.mergeSort(sortedSeq) should be (sortedSeq.sorted)
+  "MergeSort" should "correct sort mutable sequences with Int" in {
+    val arrayInteger = ArrayBuffer(1, 7, 5, 5, 12, 2, 10)
+    val seqInteger = mutable.Seq(1, 7, 5, 5, 12, 2, 10)
+    val sortedSeq = mutable.Seq(1, 2, 3)
+
+    Sorting.mergeSort(arrayInteger)
+    arrayInteger should be (arrayInteger.sorted)
+    Sorting.mergeSort(seqInteger)
+    seqInteger should be (seqInteger.sorted)
+    Sorting.mergeSort(sortedSeq)
+    sortedSeq should be (sortedSeq.sorted)
   }
-  it should "correct sort sequence with String or Char" in {
-    Sorting.mergeSort(listChar) should be (listChar.sorted)
-    Sorting.mergeSort(listString) should be (listString.sorted)
+  it should "correct sort mutable sequences with String or Char" in {
+    val arrayString = ArrayBuffer("def", "abb", "abcd", "a", "ab")
+    val arrayChar = ArrayBuffer('a', 'f', 'm', 'b', 'c', 'e', 'f')
+
+    Sorting.mergeSort(arrayChar)
+    arrayChar should be (arrayChar.sorted)
+    Sorting.mergeSort(arrayString)
+    arrayString should be (arrayString.sorted)
   }
-  it should "correct sort sequence with Person" in {
-    implicit val peopleOrdering: Ordering[Person] = Ordering.by(_.birthDate.getYear)
-    Sorting.mergeSort(people) should be (people.sorted)
+  it should "correct sort mutable sequences with Person" in {
+    implicit val peopleOrdering: Ordering[Person] = Ordering.by(_.name)
+    val people = ArrayBuffer(
+      Person("Bob", LocalDate.of(1981, 5, 12)),
+      Person("Charlie", LocalDate.of(1982, 5, 11)),
+      Person("Alice", LocalDate.of(1970, 1, 1)),
+      Person("Michael", LocalDate.of(1978, 5, 12))
+    )
+    Sorting.mergeSort(people)
+    people should be (people.sorted)
+  }
+  it should "correct sort mutable BIG sequences with Int" in {
+    val bigArrayInteger: ArrayBuffer[Int] = ArrayBuffer.fill(1000000)(util.Random.nextInt)
+    Sorting.mergeSort(bigArrayInteger)
+    bigArrayInteger should be (bigArrayInteger.sorted)
   }
 }
